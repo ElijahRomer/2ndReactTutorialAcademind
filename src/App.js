43ef15bd495e1ed7712e316e3e825c28ed3655c5
Elijah Romer <romere88@gmail.com>
2021-10-28 18:12:51 -0500

    added layout folder to components folder. Added MainNavigation.js to the layout folder to begin setting up the NavBar and creating layout components.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   //localhost:3000/
@@ -10,6 +11,7 @@ function App() {
     <div>
       {/* switch tells the router to only render one route */}
       {/* React Router Dom uses RegEx to match paths so it can render multiple views, however if you only want a route to render the path is an exact match, add the exact attribute to opening tag of the Route component in question. */}
+      <MainNavigation />
       <Switch>
         <Route path="/" exact>
           <AllMeetupsPage />
