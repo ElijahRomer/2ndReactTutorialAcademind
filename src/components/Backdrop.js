e42a330e25b1aa5e92360f9ec38dbdc07d500ed4
Elijah Romer <romere88@gmail.com>
2021-10-28 17:45:12 -0500

    set up initial tutorial state

--- src/components/Backdrop.js
+++
@@ -1,7 +0,0 @@
-function Backdrop(props) {
-  // forwarding to the parent component - two ways of passing functions to components
-  return <div className="backdrop" onClick={props.onCancel} />;
-  // can be written as a self closing tag as there is no content within the tag, even though in regular HTML this is not a self-closing tag.
-}
-
-export default Backdrop;
