883512f36c3bd35fbb67d3e9c076f806d7dac62b
Elijah Romer <romere88@gmail.com>
2021-10-28 18:01:09 -0500

    created page components and folder. Imported to App.js and then imported route and switch from react-router-dom and set up a basic router.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+
 function App() {
-  return <div></div>;
+  //localhost:3000/
+  return (
+    <div>
+      {/* switch tells the router to only render one route */}
+      {/* React Router Dom uses RegEx to match paths so it can render multiple views, however if you only want a route to render the path is an exact match, add the exact attribute to opening tag of the Route component in question. */}
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
